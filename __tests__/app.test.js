c74a3b2d0ea5b4f7b8f82ae2b25ff1aee8025eae
MorganRedgrove <morganekelunddavies@gmail.com>
2023-01-16 12:09:58 +0000

    test 1 implemented/README updated

---
+++ __tests__/app.test.js
@@ -0,0 +1,26 @@
+const request = require("supertest")
+const assert = require("supertest")
+const app = require("../app/app")
+const db = require("../db/connection")
+const seed = require("../db/seeds/seed")
+const data = require("../db/data/test-data")
+
+beforeEach(() => {
+    return seed(data)
+})
+  
+afterAll(() => {
+    return db.end()
+})
+
+describe("news-api", () => {
+    describe("GET requests", () => {
+        describe("GET /api/topics", () => {
+            test("responds with status code 200", () => {
+                return request(app)
+                .get("/api/topics")
+                .expect(200)
+            })
+        })
+    })
+})
---
+++ app/app.controller.js
@@ -0,0 +1,5 @@
+const getTopics = (request, response) => {
+    response.status(200).send()
+}
+
+module.exports = { getTopics }
