427766d1cb5959e5f3aba82208441759a61ed777
MorganRedgrove <morganekelunddavies@gmail.com>
2023-01-17 12:36:31 +0000

    query/tests refactored

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,127 +19,22 @@ afterAll(() => {
 
 describe("news-api", () => {
     describe("GET requests", () => {
-        describe("GET /api/articles", () => {
-            test("responds with status code 200", () => {
-                return request(app)
-                .get("/api/articles")
-                .expect(200)
-            })
-            test("responds with an object with a key of 'articles'", () => {
-                return request(app)
-                .get("/api/articles")
-                .expect(200)
-                .then(({body}) => {
-                    expect(body).toHaveProperty("articles")
-                })
-            })
-            test("the value of 'articles' is an array of objects", () => {
-                return request(app)
-                .get("/api/articles")
-                .expect(200)
-                .then(({body}) => {
-                    const { articles } = body
-                    expect(articles.length).toBeGreaterThan(0)
-                    expect(articles instanceof Array).toBe(true)
-                    articles.forEach((element) => {
-                        expect(element instanceof Object).toBe(true)
-                    })
-                })
-            })
-            test("each object in the 'articles' array has the expected keys", () => {
-                return request(app)
-                .get("/api/articles")
-                .expect(200)
-                .then(({body}) => {
-                    const { articles } = body
-                    expect(articles.length).toBeGreaterThan(0)
-                    articles.forEach((article) => {
-                        expect(article).toHaveProperty("author")
-                        expect(article).toHaveProperty("title")
-                        expect(article).toHaveProperty("article_id")
-                        expect(article).toHaveProperty("topic")
-                        expect(article).toHaveProperty("created_at")
-                        expect(article).toHaveProperty("votes")
-                        expect(article).toHaveProperty("article_img_url")
-                        expect(article).toHaveProperty("comment_count")
-                    })
-                })
-            })  
-            test("the value of 'comment_count' is equal the number of comments for a given article", () => {
-                return request(app)
-                .get("/api/articles")
-                .expect(200)
-                .then(({body}) => {
-                    const { articles } = body
-                    expect(articles.length).toBeGreaterThan(0)
-                    articles.forEach((article) => {
-                        const expectedCount = comments.filter((comment) => {
-                            return comment.article_id == article.article_id
-                        }).length
-                        expect(article.comment_count).toBe(expectedCount)
-                    })
-                })
-            })  
-            test("the values of 'title', 'topic', 'author', 'body', 'created_at' and 'article_img_url' are retreived from the 'nc_snack_database'", () => {
-                return request(app)
-                .get("/api/articles")
-                .expect(200)
-                .then(({body}) => {
-                    const { articles } = body
-                    expect(articles.length).toBeGreaterThan(0)
-
-                    valueStrings = expectedArticles.map((article) => {
-                        const { title, topic, author, body, created_at, article_img_url } = article
-                        return `${title},${topic},${author},${body},${Date(created_at)},${article_img_url}`
-                    })
-
-                    articles.forEach((article) => {
-                        const { title, topic, author, body, created_at, article_img_url } = article
-                        const valueString = `${title},${topic},${author},${body},${Date(created_at)},${article_img_url}`
-                        expect(valueStrings.includes(valueString)).toBe(true)
-                    })
-
-                })
-            }) 
-            test("the 'articles' array is returned in descending date order", () => {
-                return request(app)
-                .get("/api/articles")
-                .expect(200)
-                .then(({body}) => {
-                    const { articles } = body
-                    expect(articles.length).toBeGreaterThan(0)
-                    expect(articles).toBeSortedBy("created_at", {descending: true})
-                })
-            }) 
-        })
         describe("GET /api/topics", () => {
-            test("responds with status code 200", () => {
-                return request(app)
-                .get("/api/topics")
-                .expect(200)
-            })
-            test("responds with an object with a key of 'topics'", () => {
+            test("responds with status code 200 and an object in expected format", () => {
                 return request(app)
                 .get("/api/topics")
                 .expect(200)
                 .then(({body}) => {
                     expect(body).toHaveProperty("topics")
-                })  
-            })    
-            test("the value of 'topics' is an array of objects", () => {
-                return request(app)
-                .get("/api/topics")
-                .expect(200)
-                .then(({body}) => {
                     const { topics } = body
-                    expect(topics.length).toBeGreaterThan(0)
                     expect(topics instanceof Array).toBe(true)
+                    expect(topics.length).toBeGreaterThan(0)
                     topics.forEach((element) => {
                         expect(element instanceof Object).toBe(true)
                     })
-                })
-            })  
-            test("each object in the 'topics' array has a key of 'slug' and 'description'", () => {
+                })  
+            })
+            test("each object in the 'topics' array has the expected keys and value types", () => {
                 return request(app)
                 .get("/api/topics")
                 .expect(200)
@@ -147,8 +42,8 @@ describe("news-api", () => {
                     const { topics } = body
                     expect(topics.length).toBeGreaterThan(0)
                     topics.forEach((topic) => {
-                        expect(topic).toHaveProperty("slug")
-                        expect(topic).toHaveProperty("description")
+                        expect(topic.slug).toEqual(expect.any(String))
+                        expect(topic.description).toEqual(expect.any(String))
                     })
                 })
             })     
@@ -164,6 +59,68 @@ describe("news-api", () => {
                     })
                 })
             })       
+        })
+
+        describe("GET /api/articles", () => {
+            test("responds with status code 200 and an object in expected format", () => {
+                return request(app)
+                .get("/api/articles")
+                .expect(200)
+                .then(({body}) => {
+                    expect(body).toHaveProperty("articles")
+                    const { articles } = body
+                    expect(articles instanceof Array).toBe(true)
+                    expect(articles.length).toBeGreaterThan(0)
+                    articles.forEach((element) => {
+                        expect(element instanceof Object).toBe(true)
+                    })
+                })
+                
+            })
+            test("each object in the 'articles' array has the expected keys and value types", () => {
+                return request(app)
+                .get("/api/articles")
+                .expect(200)
+                .then(({body}) => {
+                    const { articles } = body
+                    expect(articles.length).toBeGreaterThan(0)
+                    articles.forEach((article) => {
+                        expect(article.author).toEqual(expect.any(String))
+                        expect(article.title).toEqual(expect.any(String))
+                        expect(article.article_id).toEqual(expect.any(Number))
+                        expect(article.topic).toEqual(expect.any(String))
+                        expect(article.created_at).toEqual(expect.any(String))
+                        expect(article.votes).toEqual(expect.any(Number))
+                        expect(article.article_img_url).toEqual(expect.any(String))
+                        expect(article.comment_count).toEqual(expect.any(String))
+                    })
+                })
+            })  
+            test("the value of 'comment_count' is equal the number of comments for a given article", () => {
+                return request(app)
+                .get("/api/articles")
+                .expect(200)
+                .then(({body}) => {
+                    const { articles } = body
+                    expect(articles.length).toBeGreaterThan(0)
+                    articles.forEach((article) => {
+                        const expectedCount = comments.filter((comment) => {
+                            return comment.article_id == article.article_id
+                        }).length
+                        expect(article.comment_count).toBe(String(expectedCount))
+                    })
+                })
+            })  
+            test("the 'articles' array is returned in descending date order", () => {
+                return request(app)
+                .get("/api/articles")
+                .expect(200)
+                .then(({body}) => {
+                    const { articles } = body
+                    expect(articles.length).toBeGreaterThan(0)
+                    expect(articles).toBeSortedBy("created_at", {descending: true})
+                })
+            }) 
         })
     })
 })
--- app/app.model.js
+++ app/app.model.js
@@ -2,16 +2,13 @@ const db = require("../db/connection")
 
 const selectArticles = () => {
     return db.query(`
-        SELECT articles.*, 
-        COALESCE (comment_count.count :: INT, 0) AS comment_count
+        SELECT articles.*,
+        COUNT(comments.comment_id) AS comment_count
         FROM articles
-        LEFT OUTER JOIN
-        (
-        SELECT article_id, COUNT(*)
-        FROM comments
-        GROUP BY article_id
-        ) comment_count
-        ON articles.article_id = comment_count.article_id
+        LEFT JOIN
+        comments
+        ON articles.article_id = comments.article_id
+        GROUP BY articles.article_id
         ORDER BY articles.created_at DESC
         `)
     .then((result) => {
